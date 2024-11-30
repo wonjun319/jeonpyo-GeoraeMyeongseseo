c94b4f178affea969284028f03ed4c485bc7924e
wonjun seo <87961183+wonjun319@users.noreply.github.com>
2024-11-30 11:10:27 +0900

    Update jeonpyo.py

--- jeonpyo.py
+++ jeonpyo.py
@@ -8,7 +8,7 @@ import openpyxl
 import pandas as pd
 
 file_path = '데이터.xlsx'
-
+file_path2 = r'C:\Users\user\dist\거래명세서및거래명세표.xlsx'
 # Read the .xlsx file
 df = pd.read_excel(file_path)
 
@@ -20,6 +20,10 @@ def save_data():
     you = you_var.get()  # StringVar를 사용하여 you_entry의 값 가져오기
     mock = mock_var.get() 
     num = int(num_entry.get())
+    try:
+        pay = int(pay_result.get())
+    except (ValueError, AttributeError):
+        pay = 0
 
     # XLSX 파일 이름과 경로 설정
     if you in yous:
@@ -40,12 +44,13 @@ def save_data():
         tot2 = tot + totprice
     else:
         tot2 = totprice  # 파일이 없으면 tot2는 totprice와 동일
-
+    retot = tot2 - pay
+    # jutot = df2['잔금'].iloc[-1] + totprice
     # 저장할 데이터 (날짜 포함)
-    data = [you, mock, num, price, totprice, tot2, datetime.now().strftime('%Y-%m-%d')]
+    data = [you, mock, num, price, totprice, tot2, pay, retot, datetime.now().strftime('%Y-%m-%d')]
 
-    # 컬럼명 설정
-    columns = ['거래처', '품목', '수량', '단가', '공급가액', '잔금', '거래날짜']
+    # 컬럼명 설정 
+    columns = ['거래처', '품목', '수량', '단가', '공급가액', '합계', '입금', '잔금', '거래날짜']
 
     # XLSX 파일이 이미 존재하는지 확인
     try:
@@ -67,36 +72,54 @@ def save_data():
 
     # 파일 저장 완료 메시지 출력 (옵션)
     print(f"{file_name}에 데이터가 저장되었습니다.")
-    file_path = r'C:\Users\user\dist\거래명세서및거래명세표.xlsx'
-    wb1 = openpyxl.load_workbook(file_path)
-    sheet = wb1.active
-    sheet['D4'] = datetime.now().strftime('%Y-%m-%d')
-    sheet['D5'] = you
-    sheet['D7'] = totprice
-    sheet['D10'] = mock
-    sheet['I10'] = num
-    sheet['M10'] = price
-    sheet['P10'] = totprice
-    sheet['E21'] = tot
-    sheet['P21'] = tot2
-    wb1.save(file_path)
-
     
+    wb1 = openpyxl.load_workbook(file_path2)
+    sheet = wb1.active
+    sheet['G31'] = datetime.now().strftime('%Y-%m-%d')
+    sheet['G35'] = you
+    sheet['G37'] = totprice
+    sheet['C40'] = mock
+    sheet['R40'] = num
+    sheet['V40'] = price
+    sheet['Z40'] = totprice
+    sheet['G50'] = tot
+    sheet['Q50'] = tot2
+    sheet['AB50'] = pay
+    sheet['AB52'] = retot
+    sheet['G4'] = datetime.now().strftime('%Y-%m-%d')
+    sheet['G8'] = you
+    sheet['G10'] = totprice
+    sheet['C13'] = mock
+    sheet['R13'] = num
+    sheet['V13'] = price
+    sheet['Z13'] = totprice
+    sheet['G23'] = tot
+    sheet['Q23'] = tot2
+    sheet['AB23'] = pay
+    sheet['AB25'] = retot
+    wb1.save(file_path2)
+    print_button.grid(row=3, column=1, padx=5, pady=5)
+
+def startprint():
     excel = win32com.client.Dispatch("Excel.Application")
     excel.Visible = False
-    workbook = excel.Workbooks.Open(file_path)
+    workbook = excel.Workbooks.Open(file_path2)
     workbook.PrintOut()
     excel.Quit()
+
 def fine_data():
     fineyou = you_var.get()
-
+    mock = mock_var.get() 
+    num = int(num_entry.get())
     if fineyou in yous:
         find_name = f'{fineyou}거래서.xlsx'
     else:
         find_name = '기타거래서.xlsx'
-
+    if mock in mocks:
+        price = prices[mocks.index(mock)]  # 가격 설정
+    totprice = price * num 
     df2 = pd.read_excel(find_name)
-    tot2 = df2['잔금'].iloc[-1]
+    tot2 = df2['잔금'].iloc[-1] + totprice
 
     fine_result.config(text=f"{tot2}") 
     return tot2
@@ -109,31 +132,33 @@ def pay_data():
     result_result.config(text=f"{result}")
     you = you_var.get()
 
-    # XLSX 파일 이름과 경로 설정
-    if you in yous:
-        file_name = f'{you}거래서.xlsx'
-    else:
-        file_name = '기타거래서.xlsx'
-    df2 = pd.read_excel(file_name)
-    columns = ['거래처', '품목', '수량', '단가', '공급가액', '잔금', '거래날짜']
-    data = [you, None, None, None, None, result, datetime.now().strftime('%Y-%m-%d')]
-    try:
-        # 기존 엑셀 파일을 열기
-        wb = openpyxl.load_workbook(file_name)
-        sheet = wb.active
-    except FileNotFoundError:
-        # 파일이 없다면 새로 생성
-        wb = openpyxl.Workbook()
-        sheet = wb.active
-        sheet.append(columns)
-    # 데이터 추가 (컬럼에 맞는 순서로 데이터가 들어가도록)
-    sheet.append(data)
+    # # XLSX 파일 이름과 경로 설정
+    # if you in yous:
+    #     file_name = f'{you}거래서.xlsx'
+    # else:
+    #     file_name = '기타거래서.xlsx'
+    # df2 = pd.read_excel(file_name)
+    # columns = ['거래처', '품목', '수량', '단가', '공급가액', '잔금', '거래날짜']
+    # data = [you, None, None, None, None, result, datetime.now().strftime('%Y-%m-%d')]
+    # try:
+    #     # 기존 엑셀 파일을 열기
+    #     wb = openpyxl.load_workbook(file_name)
+    #     sheet = wb.active
+    # except FileNotFoundError:
+    #     # 파일이 없다면 새로 생성
+    #     wb = openpyxl.Workbook()
+    #     sheet = wb.active
+    #     sheet.append(columns)
+    # # 데이터 추가 (컬럼에 맞는 순서로 데이터가 들어가도록)
+    # sheet.append(data)
 
-    # XLSX 파일에 저장
-    wb.save(file_name)
+    # # XLSX 파일에 저장
+    # wb.save(file_name)
 
-    # 파일 저장 완료 메시지 출력 (옵션)
-    print(f"{file_name}에 데이터가 저장되었습니다.")
+    # # 파일 저장 완료 메시지 출력 (옵션)
+    # print(f"{file_name}에 데이터가 저장되었습니다.")
+
+
 root = tk.Tk()
 # StringVar로 OptionMenu에 선택된 값 관리
 
@@ -163,7 +188,8 @@ num_entry = ttk.Entry(root)
 
 # 저장 버튼 생성
 fine_button = ttk.Button(root, text='조회', command=fine_data)
-save_button = ttk.Button(root, text='저장 및 인쇄', command=save_data)
+save_button = ttk.Button(root, text='저장', command=save_data)
+print_button = ttk.Button(root, text='인쇄', command=startprint)
 fine_label = ttk.Label(root, text='총액:')
 fine_result = ttk.Label(root, text='　　　　　　　　')
 pay_button = ttk.Button(root, text='지불', command=pay_data)
@@ -189,10 +215,9 @@ result_label.grid(row=2, column=3, padx=5, pady=5)
 result_result.grid(row=2, column=4, padx=5, pady=5)
 
 # 저장 버튼 위치 지정
-save_button.grid(row=3, column=1, padx=5, pady=5)
+save_button.grid(row=3, column=4, padx=5, pady=5)
 
 
 
-root.mainloop()
 
 root.mainloop()
